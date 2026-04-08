0b2713830924dc3fb52e000e9650f715d8dbc59b
Julik52 <paskovaulia31@stud.op.edu.ua>
2026-04-08 17:28:09 +0300

    Initial commit Lab1 Parshikova AI-231

---
+++ Object-Oriented Programming in Kotlin/Sealed Classes/src/Task.kt
@@ -0,0 +1,4 @@
+// TODO: Створіть sealed class NetworkResult і два дата-класи: Success та Error, що наслідують його
+sealed class NetworkResult
+data class Success(val data: String) : NetworkResult()
+data class Error(val errorMsg: String) : NetworkResult()
